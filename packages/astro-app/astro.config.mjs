6502dc15c2296a1333f3a538fe6c0ede5ae806f4
ciszewsb <bartlomiej.ciszewski@tomtom.com>
2024-04-16 11:08:58 +0200

    feat: add mfe

--- packages/astro-app/astro.config.mjs
+++ packages/astro-app/astro.config.mjs
@@ -1,27 +1,20 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import federation from "@originjs/vite-plugin-federation";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+
+import moduleFederation from "astro-module-federation";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()],
-  vite: {
-    build: {
-      target: "esnext",
+  integrations: [react(), tailwind(), moduleFederation({
+    remotes: {
+      viteApp: "http://localhost:4173/assets/remoteEntry.js"
     },
-    plugins: [
-      federation({
-        remotes: {
-          viteApp: "http://localhost:4173/assets/remoteEntry.js",
-        },
-        // shared: ['react', 'react-dom']
-      }),
-    ],
-  },
+    shared: ['react', 'react-dom']
+  })],
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
